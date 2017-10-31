0081ec1c8fee44d5ffacc9ec4ae6d05ccebf30d7
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-10-31 19:17:48 +0900

    postGame
    ゴミ箱にボールを入れるゲームです。

---
+++ Assets/shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+	public float power;
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			rb.AddForce (transform.forward * power);
+		}
+	}
+}
